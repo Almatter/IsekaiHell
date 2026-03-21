2e5109bd4c19042333ab1067fbe319c1315d281c
Almatter <133932226+Almatter@users.noreply.github.com>
2026-03-21 15:03:17 -0400

    Remove calculatePoints invocation in title logic
    Remove calculatePoints call after rendering titles.

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ $(document).ready(function() {
  //title logic
   populateTitleDropdown();
     renderSelectedTitles();
-    calculatePoints();
+
 
     $("#titleSelect").on("change", function () {
       updateSelectedDescription();
